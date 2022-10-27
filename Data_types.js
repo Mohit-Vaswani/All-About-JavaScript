b5f6b96cfe6169991936a06fd7e55d0512ff7de0
Mohit Vaswani <mohitvaswani010@gmail.com>
2022-10-27 13:47:43 +0530

    Data-types file added

---
+++ Data_types.js
@@ -0,0 +1,26 @@
+// Data Types in JavaScript
+
+// 1. Values used in your code can be of certain type - number or string for example.
+// 2. This type is called data type of the language.
+// 3. Data Types supported in JavaScript are: Number, String, Boolean, Function, Object, Null, and Undefined.
+// 4. They are categorized as primitive or non-primitive data types.
+
+// Data type has 2 categories
+
+// 1. Primitive: Number, String, Boolean, Null, Undefined
+
+// 1. Non-Primitive: Array, Function, Object
+
+// 1. Unlike Java or C#, JavaScript is a loosely-typed language.
+// 2. No type declarations are required when variables are created.
+// 3. Data Types are important in a programming language to perform operations on the variables.
+
+// Data Types examples
+
+var x = 10; // number variable
+var x = "hi"; // string variable
+var x = true; // boolean variable
+function x() {/*your function code here*/} // function variable
+var x = {}; // object variable
+var x = null; // null variable
+var x; // undefined variable
